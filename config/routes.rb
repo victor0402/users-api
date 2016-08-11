2acdb05a497c65a2ffa8b12ee7fef45f0df9444e
victor0402 <vcarvalho0402@gmail.com>
2016-08-11 09:33:10 -0300

    options route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
+      match 'users/:id', to: 'users#destroy', via: [:options], as: :destroy_user_via_options
     end
   end
 end
